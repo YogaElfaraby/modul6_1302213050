c88c863aefb70036757216706690f05958cb1c1a
unknown <yoga85ananta@gmail.com>
2023-03-29 17:16:38 +0700

    Menambahkan dua class SayaTubeVideo dan SayaTubeUser

--- modul6_1302213050/modul6_1302213050/Program.cs
+++ modul6_1302213050/modul6_1302213050/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace modul6_1302213050
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            SayaTubeVideo vidio;
+            SayaTubeUser YogaAnantaElfaraby = new SayaTubeUser("YogaAnantaElfaraby");
+            Random rnd = new Random();
+
+
+
+            String[] Movie = new string[]
+            {
+            "Dora The Explorer",
+            "Upin Ipin bertemu opet",
+            "Gary kabur dari rumah",
+            "Kaori Tengkorak hidup",
+            "Tenki no Ko",
+            "Tontonan Wibu",
+            "Rambo Ayam Sadboy",
+            "Ultraman Ribut",
+            "Berserk",
+            "Vinland Saga",
+            };
+
+        }
+    }
+}
